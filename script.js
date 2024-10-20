39069a2cbad5b4273914dc8f2dc0dac28c83c496
qingfengshiying <1992chentao@gmail.com>
2024-10-20 16:41:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,276 +1,88 @@
-// define: colors
-const colorFeatures = "#AB5C53";
-const black         = "#211F1F";
-const colorShadow   = "#C39B88";
-const colorSkin     = "#E5C0AA";
-const white         = "#FFF";
+var textureURL = "img/lroc_color_poles_1k.jpg"; 
+var displacementURL = "img/ldem_3_8bit.jpg"; 
+var worldURL = "img/hipp8_s.jpg"
 
-// define: helpers
-var isSpinning = true;
-var TAU = Zdog.TAU;
+var scene = new THREE.Scene();
 
-// define: illustration
-var mona = new Zdog.Illustration({
-  element: ".mona",
-  dragRotate: true,
-  onDragStart: function() {
-    isSpinning = false;
-  },
-});
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// model: head
-const head = new Zdog.Anchor({
-  addTo: mona,
-	translate: {
-		y: 15
-	},
-});
+var renderer = new THREE.WebGLRenderer();
 
-const domepiece = new Zdog.Group({
-  addTo: head
-});
-
-const noggin = new Zdog.RoundedRect({
-  addTo: domepiece,
-  width: 160,
-  height: 66,
-  stroke: 228,
-  cornerRadius: 25,
-  color: black,
-  path: [
-    { x: -4.5 },
-    { x: 5.5 }
-  ]
-});
-
-const face = new Zdog.Group({
-  addTo: head,
-  translate: { 
-    x: 0, 
-    y: 36, 
-    z: 20 
-  },
-});
-
-const skinShadow = new Zdog.RoundedRect({
-  addTo: face,
-  width: 100,
-  height: 0,
-  stroke: 180,
-  cornerRadius: 40,
-  color: colorShadow,
-});
-
-const skin = new Zdog.RoundedRect({
-  addTo: face,
-  width: 100,
-  height: 0,
-  stroke: 170,
-  cornerRadius: 40,
-  color: colorSkin,
-  translate: {
-    y: 4.5
-  }
-});
-
-// model: eyes
-const eyeRight = new Zdog.Anchor({
-  addTo: face,
-  translate: { 
-    x: -76, 
-    y: 6, 
-    z: 80 
-  },
-  rotate: { 
-    y: TAU / 14 
-  }
-});
-
-const eye = new Zdog.Group({
-  addTo: eyeRight
-});
-
-const iris = new Zdog.Ellipse({
-  addTo: eye,
-  fill: true,
-  width: 40,
-  height: 56,
-  stroke: 2,
-  scale: 1.5,
-  color: white,
-});
-
-const pupil = new Zdog.Ellipse({
-  addTo: eye,
-  width: 37,
-  height: 56,
-  stroke: 0,
-  fill: true,
-  color: colorFeatures,
-  translate: { 
-    x: 3, 
-    y: 5, 
-    z: 0 
-  },
-});
-
-const 반짝반짝 = new Zdog.Ellipse({
-  addTo: pupil,
-  width: 10,
-  height: 10,
-  color: white,
-  fill: true,
-  stroke: 0,
-  translate: { 
-    x: -7, 
-    y: -12, 
-    z: 3 
-  }
-});
-
-const eyeLeft = eyeRight.copyGraph({
-  translate: { 
-    x: 76, 
-    y: 6, 
-    z: 80 
-  },
-  rotate: { 
-    y: TAU / -14 
-  }
-});
-
-// model: nose
-const nose = new Zdog.Ellipse({
-  addTo: face,
-  width: 6,
-  height: 6,
-  fill: true,
-  stroke: 10,
-	color: colorFeatures,
-  translate: { 
-    x: 0, 
-    y: 32, 
-    z: 74 
-  },
-});
-
-// model: mouth
-const mouth = new Zdog.Ellipse({
-  addTo: face,
-	diameter: 30,
-	quarters: 2,
-	stroke: 4,
-  scale: { 
-    x: 0.8, 
-    y: 1 
-  },
-	color: colorFeatures, 
-  rotate: { 
-    x: TAU / 2.3, 
-    z: TAU / -4 
-  },
-  translate: { 
-    x: 0, 
-    y: 46, 
-    z: 74 
-  },
-});
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.enablePan = false;
 
 
-// model: ears
-const ear = new Zdog.Cone({
-  addTo: head,
-  diameter: 120,
-  length: 90,
-  stroke: false,
-  color: black,
-	translate: {
-		x: -120,
-		y: -105
-	},
-  rotate: {
-    x: TAU/4,
-    y: TAU/12
-  },
-});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-ear.copy({
-  translate: { 
-    x: 120, 
-    y: -105 
-  },
-  rotate: {
-    x: TAU/4,
-    y: TAU/-12
-  },
-});
+var geometry = new THREE.SphereGeometry( 2,60,60 );
+
+var textureLoader = new THREE.TextureLoader();
+var texture = textureLoader.load( textureURL );
+var displacementMap = textureLoader.load( displacementURL );
+var worldTexture = textureLoader.load( worldURL );
+
+var material = new THREE.MeshPhongMaterial ( 
+  { color: 0xffffff ,
+  map: texture ,
+     displacementMap: displacementMap,
+  displacementScale: 0.06,
+  bumpMap: displacementMap,
+  bumpScale: 0.04,
+   reflectivity:0, 
+   shininess :0
+  } 
+
+);
+
+var moon = new THREE.Mesh( geometry, material );
 
 
-// model: whiskies
-const whiskersRight = new Zdog.Anchor({
-  addTo: head,
-  translate: { 
-    x: -290, 
-    y: 20
-  },
-});
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(-100, 10,50);
+scene.add(light);
 
-const whiskers = new Zdog.Group({
-  addTo: whiskersRight
-});
 
-const whisker = new Zdog.Shape({
-	addTo: whiskers,
-	path: [
-		{ x: 100, y: 0 },
-    { arc: [
-      { x: 30, y: -10 }, // corner
-      { x: 0, y: 0 }, // end point
-    ]}
-	],
-	closed: false,
-	stroke: 4,
-	color: black,
-});
+hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.1 );
+hemiLight.color.setHSL( 0.6, 1, 0.6 );
+hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+hemiLight.position.set( 0, 0, 0 );
+scene.add( hemiLight );
 
-whisker.copy ({
-	path: [
-		{ x: 100, y: 0 },
-    { arc: [
-      { x: 30, y: -5 }, // corner
-      { x: 0, y: 10 }, // end point
-    ]}
-	],
-  translate: { 
-    y: 20
-  },
-});
 
-const whiskersLeft = whiskersRight.copyGraph({
-  translate: { 
-    x: 290, 
-    y: 20
-  },
-  rotate: {
-    y: TAU/2,
-  },
-});
+var worldGeometry = new THREE.SphereGeometry( 1000,60,60 );
+var worldMaterial = new THREE.MeshBasicMaterial ( 
+  { color: 0xffffff ,
+  map: worldTexture ,
+  side: THREE.BackSide
+  } 
+);
+var world = new THREE.Mesh( worldGeometry, worldMaterial );
+scene.add( world );
 
-// render + animate
-var t = 0;
-var tSpeed = 1/240;
+scene.add( moon );
+camera.position.z = 5;
+
+moon.rotation.x = 3.1415*0.02;
+moon.rotation.y = 3.1415*1.54;
+
 
 function animate() {
-  if ( isSpinning ) {
-    t += tSpeed;
-    var theta = Zdog.easeInOut( t % 1 ) * TAU;
-    var delta = TAU * -3/64;
-    mona.rotate.y = Math.sin( theta ) * delta;
-    mona.rotate.x = ( Math.cos( theta ) * -0.75 + -0.5 ) * delta;
-  }
+	requestAnimationFrame( animate );
+  moon.rotation.y += 0.002;
+  moon.rotation.x += 0.0001;
+  world.rotation.y += 0.0001
+  world.rotation.x += 0.0005
 
-  mona.updateRenderGraph();
-  requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
+
+
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-animate();
+window.addEventListener('resize', onResize, false);
